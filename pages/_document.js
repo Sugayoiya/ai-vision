36bcba0641474ba34ab046a03c58374d4ad77842
sugayoiya <no_moko@icloud.com>
2024-01-25 09:48:42 +0800

    :sparkles: add clarity

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Head, Html, Main, NextScript} from 'next/document'
 
 export default class MyDocument extends Document {
     render() {
@@ -12,19 +12,17 @@ export default class MyDocument extends Document {
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "kfx5eatp07");
+    })(window, document, "clarity", "script", "kqtqljq0re");
               `
                         }}
                     />
-                    <script
-                        async
-                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6736001059793962"
-                        crossorigin="anonymous"
-                    ></script>
+                    <script async
+                            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2803739586881684"
+                            crossorigin="anonymous"></script>
                 </Head>
                 <body>
-                    <Main />
-                    <NextScript />
+                <Main/>
+                <NextScript/>
                 </body>
             </Html>
         )
